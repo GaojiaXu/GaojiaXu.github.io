fc21676ca8d7edf1962a36d9800c5eff93841fd7
Gaojia Xu <gx22@duke.edu>
2021-09-03 16:37:47 -0400

    change config

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,14 +19,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'https://getpelican.com/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Assignment.md', 'https://github.com/cliburn/bios-823-2021/blob/main/homework/assignments.md'),)
 
 DEFAULT_PAGINATION = 10
 
